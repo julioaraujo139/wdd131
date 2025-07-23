8c223a984986cce53f349eec58c416185615dd83
julioaraujo139 <jaraujo.byupathway@outlook.com>
2025-07-22 23:30:20 -0300

    final commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,12 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerBtn = document.getElementById("hamburgerBtn");
+    const nav = document.getElementById("hamburger");
+
+    hamburgerBtn.addEventListener("click", () => {
+        nav.classList.toggle("open");
+        hamburgerBtn.textContent = nav.classList.contains("open") ? "✖" : "☰";
+    });
+});
